e8050dad5aa1467490d4127e83496672b2780d55
anrigu <42425774+anrigu@users.noreply.github.com>
2018-10-07 17:05:10 -0400

    How to use string resource

--- ButtonTest/app/src/main/java/com/example/anrigu/buttontest/PushButtonTest.java
+++ ButtonTest/app/src/main/java/com/example/anrigu/buttontest/PushButtonTest.java
@@ -2,6 +2,7 @@ package com.example.anrigu.buttontest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,12 +13,13 @@ public class PushButtonTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_button_test);
     }
-    public void change_text(View view) {
-        TextView hello = (TextView) findViewById(R.id.hello);
-        if (hello.getText() == "Hello World!") {
-            hello.setText("Bye World!");
+    public void changeText(View view) {
+        TextView greeting_text = findViewById(R.id.hello);
+        Log.i("MyInfo","Button tapped");
+        if (greeting_text.getText() == getString(R.string.hello)) {
+            greeting_text.setText(getString(R.string.bye));
         } else {
-            hello.setText("Hello World!");
+            greeting_text.setText(getString(R.string.hello));
         }
     }
 }
