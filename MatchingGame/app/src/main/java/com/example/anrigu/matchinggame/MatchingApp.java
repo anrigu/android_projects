ac2992fe5672af2a5b7f7372dc358cec7155ce1b
anrigu <42425774+anrigu@users.noreply.github.com>
2018-10-08 22:17:01 -0400

    Layout fix

--- MatchingGame/app/src/main/java/com/example/anrigu/matchinggame/MatchingApp.java
+++ MatchingGame/app/src/main/java/com/example/anrigu/matchinggame/MatchingApp.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MatchingApp extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MatchingApp extends AppCompatActivity {
+    List<String> cardList = new ArrayList<>();
+    int numberOfCards = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listGenerate();
         setContentView(R.layout.activity_matching_app);
-    }
 
+    }
+    public void listGenerate(){
+        cardList.clear();
+        for(int i = 0; i < numberOfCards; i++) {
+            cardList.add(Integer.toString(random(1,2)));
+
+        }
+    }
+    public int random(int low, int high){
+        return (int) (Math.random() * (((high-low)+1))+low);
+    }
     public void clickButton(View view) {
         flipButton((Button) view);
         Button buttonOne = findViewById(R.id.button);
@@ -22,15 +37,14 @@ public class MatchingApp extends AppCompatActivity {
 
     public void flipButton(Button button) {
         if (button.getId() == R.id.button) {
-            button.setText(getString(R.string.button1));
+            button.setText(cardList.get(0));
         } else {
-            button.setText(getString(R.string.button2));
+            button.setText(cardList.get(1));
         }
     }
 
     public void checkButtons(Button button1, Button button2) {
         if (button1.getText().equals("Button") || button2.getText().equals("Button")) {
-            return;
         } else {
             if (button1.getText().equals(button2.getText())) {
                 Button button3 = findViewById(R.id.button3);
@@ -47,5 +61,6 @@ public class MatchingApp extends AppCompatActivity {
     public void clear(View view){
         ((Button)findViewById(R.id.button)).setText("Button");
         ((Button)findViewById(R.id.button2)).setText("Button");
+        listGenerate();
     }
 }
